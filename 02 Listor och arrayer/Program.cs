40de61744e29deb8c651182b40ca3d4a84a4c6c8
Calin Alexuta <calin.alexuta@elev.ga.ntig.se>
2024-11-19 13:21:17 +0100

    first commit

---
+++ 02 Listor och arrayer/Program.cs
@@ -0,0 +1,21 @@
+﻿//     string[] toys = { "Playstation", "Lego", "SpiderMan", "Rubiks Kub", "Nintendo" };
+
+//     for (int i = 0; i < toys.Length; i++)
+// {
+//     Console.WriteLine(toys[i]);
+// }
+
+//    string[] classmates = { "Alex", "Ovidiu", "Coroian", "Sobolan", "Vadim" };
+
+//     for (int i = 0; i < classmates.Length; i++)
+// {
+//     Console.WriteLine(classmates[i]);
+// }
+
+
+
+
+
+
+
+Console.ReadLine();
